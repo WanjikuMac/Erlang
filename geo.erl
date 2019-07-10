308b854206a80ad35b3d8e4ee7f0ca8a99958992
WanjikuMac <edwardscaroline32@gmail.com>
2019-07-10 09:39:30 +0300

    learning some more erlang

--- geo.erl
+++ geo.erl
@@ -1,15 +1,15 @@
 - module(geo).
 - export([for/3, show/1, parse_name/1, parse_name_new/1, sum/1, test/1, map/2, cost/1,
-  qsort/1, pytha/1, perms/1, area/1, startin/1, intersperse/2, zero_to_o/1, to_truncate/1, trim_after/2,
-  trim_after_flat/2, flatten/1, value/2]).
+          qsort/1, pytha/1, perms/1, area/1, startin/1, intersperse/2, zero_to_o/1, to_truncate/1, trim_after/2,
+          trim_after_flat/2, flatten/1, value/1, many/1, greet/2, head/1, same/2, valid_time/1, old/1, fine_if/0]).
 
 for(Max, Max, F) -> [F(Max)];
 for(I, Max, F) -> [F(I) | for(I+1,Max, F)].
 
-%sum([P]) ->
-%    [H| T] -> [H + T];
- %   [] -> 0
-  %end.
+                                                %sum([P]) ->
+                                                %    [H| T] -> [H + T];
+                                                %   [] -> 0
+                                                %end.
 
 cost(oranges) -> 3;
 cost(apples) -> 4;
@@ -19,40 +19,38 @@ cost(mangoes) -> 5.
 sum([H|T]) -> H + sum(T);
 sum([]) -> 0.
 
-%sum([H|T]) -> [H + sum(T)];
- % sum([]) -> 0.
+                                                %sum([H|T]) -> [H + sum(T)];
+                                                % sum([]) -> 0.
 
-%Returns the output of the written function after sorting the list.
+                                                %Returns the output of the written function after sorting the list.
 map(_, []) -> [];
 map(F, [H|T]) -> [F(H) | map(F, T)].
 
 
 test(Buy) ->
-  %Buy = [{oranges,1},{bananas,2},{mangoes,3}],
-  F = [cost(A)* B || {A, B} <- Buy],
-  %F = cost((A) * B || {A, B} <- Row),
-  lists:sum(F).
+                                                %Buy = [{oranges,1},{bananas,2},{mangoes,3}],
+    F = [cost(A)* B || {A, B} <- Buy],
+                                                %F = cost((A) * B || {A, B} <- Row),
+    lists:sum(F).
 
 pytha(N) ->
-  [{A,B,C} ||
-      A <- lists:seq(1,N),
-      B <- lists:seq(1, N),
-      C <- lists:seq(1, N),
-    A + B + C =< N,
-    A*A+B*B == C*C
-  ].
-
-
-  %area({circle, radius}) -> math:pi()* radius* radius;
-  %area({triangle, base, height}) -> 1/2 * base *height;
-  %area({perimeter, length, length}) -> length + length.
+    [{A,B,C} ||
+        A <- lists:seq(1,N),
+        B <- lists:seq(1, N),
+        C <- lists:seq(1, N),
+        A + B + C =< N,
+        A*A+B*B == C*C
+    ].
+                                                %area({circle, radius}) -> math:pi()* radius* radius;
+                                                %area({triangle, base, height}) -> 1/2 * base *height;
+                                                %area({perimeter, length, length}) -> length + length.
 
 area({Object, Value}) ->
-  case {Object,Value} of
-  {circle, Value} -> math:pi()* Value* Value;
-  %{triangle, Value, height} -> 1/2 * base *height;
-  {perimeter, Value} -> Value + Value
-  end.
+    case {Object,Value} of
+        {circle, Value} -> math:pi()* Value* Value;
+                                                %{triangle, Value, height} -> 1/2 * base *height;
+        {perimeter, Value} -> Value + Value
+    end.
 
 perms([]) -> [[]];
 perms(L) -> [[H | T] || H <- L, T <- perms(L--[H])].
@@ -64,27 +62,32 @@ show([P]) ->
 
 qsort([]) -> [];
 qsort([Pivot | T]) -> qsort([X || X <- T, X< Pivot])
-    ++ [Pivot] ++
-    qsort([X || X <- T, X>= Pivot]).
+                          ++ [Pivot] ++
+                          qsort([X || X <- T, X>= Pivot]).
 
 parse_name(Raw) ->
-  Tokens = string:tokens(string:trim(Raw), " "),
-  case Tokens of
-    [RawName | _] -> [H | T] = RawName,
-                     [string:to_upper(H) | string:to_lower(T)];
-    [] -> "Rafiki"
-  end.
+    Tokens = string:tokens(string:trim(Raw), " "),
+    case Tokens of
+        [RawName | _] -> [H | T] = RawName,
+                         [string:to_upper(H) | string:to_lower(T)];
+        [] -> "Rafiki"
+    end.
 
-%parse_name_new(Raw) ->
- % Tokens = string:tokens(string:trim(Raw), " "),
-  %ToFilter = lists:map(fun string:casefold/1,["I","am", "Mimi", ".", "jina", "Langu" "ni", "naitwa", "," "@", "+",
-   % "1", "2", "3", "4", "5", "6", "7", "8","9", "my", "name", "is","$", "jiunge","join", "welcome", "yes"] ),
-  %Filtered_List = lists:filter(fun(X) -> not lists:member(string:casefold(X), ToFilter)end, Tokens),
-  %case Filtered_List of
-   % [RawName | _] -> [H| T] =  RawName,
-    %  [string:to_upper(H) | string:to_lower(T)];
-    %[] -> "Rafiki"
-  %end.
+                                                %parse_name_new(Raw) ->
+                                                % Tokens = string:tokens(string:trim(Raw), " "),
+                                                %ToFilter = lists:map(fun string:casefold/1,["I","am", "Mimi", ".", "jina", "Langu" "ni", "naitwa", "," "@", "+",
+                                                % "1", "2", "3", "4", "5", "6", "7", "8","9", "my", "name", "is","$", "jiunge","join", "welcome", "yes"] ),
+                                                %Filtered_List = lists:filter(fun(X) -> not lists:member(string:casefold(X), ToFilter)end, Tokens),
+                                                %case Filtered_List of
+                                                % [RawName | _] -> [H| T] =  RawName,
+                                                %  [string:to_upper(H) | string:to_lower(T)];
+                                                %[] -> "Rafiki"
+                                                %end.
+                                                %Test this data
+                                                %"""Gideon.""",
+                                                %"""Peter\n""",
+                                                %"""Karibu kwa MoA-INFO, huduma ya mawaidha ya Wizara ya Kilimo. Huduma hii ni ya kutuma na kupokea ujumbe bila malipo. Jina lako la kwanza ni nani""",
+                                                %"""Welcome to MoA-INFO, the Ministry of Agriculture's information service. It's free to send and receive every SMS. What is your first name""",
 
 parse_name_new(Raw) ->
     TruncateAfter = [$!, $", $#, $$, $%, $&, $(, $), $*, $+, $-, $/, $:, $;, $<, $=, $>, $?, $@, $], $., $\\],
@@ -94,102 +97,180 @@ parse_name_new(Raw) ->
                                                 %Return the string without any member of the list below
     ToFilter = lists:map(fun string:casefold/1, ["I", "am", "Mimi", "jina", "Langu" "ni", "naitwa",
                                                  "1", "2", "3", "4", "5", "6", "7", "8", "9", "my", "name", "is", "jiunge", "join", "welcome", "yes", "40130", ""]),
-    [H | _T] = lists:filter(fun(X) -> not lists:member(string:casefold(X), ToFilter) end, Tokens),
-    Value = lists:flatten(string:replace(H, "0", "o")),
-    case Value of
-        [Head | Tail] ->
-            [string:to_upper(Head) | string:to_lower(Tail)];
-
-        [] -> ("Rafiki~n")
+    FilteredList= lists:filter(fun(X) -> not lists:member(string:casefold(X), ToFilter) end, Tokens),
+                                                %Remove any integer from the filtered list
+    DropInteger = lists:filter(fun (X) -> not lists:all(fun (A) -> A >= $0 andalso A =< $9 end, X)end, FilteredList),
+    io:format("~p~n", [DropInteger]),
+                                                %V = length(DropInteger),
+    case DropInteger of
+        [[_W]| _Z ] -> "Rafiki";
+        [H | _T] -> [Head|Tail] = lists:flatten(string:replace(H, "0", "o")),
+                    [string:to_upper(Head) | string:to_lower(Tail)];
+        [] -> "Rafiki"
     end.
 
+greet(male, Name) ->
+    io:format("Hello, Mr. ~s!~n", [Name]);
+greet(female, Name) ->
+    io:format("Hello, Mrs. ~s!~n", [Name]);
+greet(_, Name) ->
+    io:format("Hello, ~s!", [Name]).
 
-%function to loop through all the symbols to truncate after
-value(Raw, TrimAfter) ->
-  lists:takewhile(fun(X) -> not lists:member(X, TrimAfter) end, Raw).
-  %(lists:map(fun (X) -> case trim_after(Raw, X) == Raw of
-                   % true -> [];
-                    %false -> trim_after(Raw, X)
-                   % end
-               % end, TrimAfter)).
+head([_I, H| _T]) -> H.
+
+%guards
+old(X) when X >= 16, X =< 104 -> true;
+old(_) -> false.
+
+%if expression
+fine_if() ->
+  if 1 =:=1 ->
+    works
+  end,
+  if 1 =:=2,  1=:= 1 ->
+    works
+  end,
+  if 1 =:= 2, 1 =:= 1 ->
+    false
+  end.
+same(X,X) ->
+    true;
+same(_,_) ->
+    false.
+valid_time({Date ={Y,M,D}, Time = {H,Min,S}}) ->
+  io:format("The date tuple (~p) says today is: ~p/~p/~p,~n", [Date,Y,M,D]),
+  io:format("The time tuple (~p) indicates: ~p:~p:~p~n", [Time, H,Min,S]);
+valid_time(_) ->
+  io:format("Stop feeding me wrong data!~n").
+
+many(X) ->
+    case X of
+        [] ->
+            none;
+        [ _One, _Two] ->
+            two;
+        [ _One ] ->
+            one;
+        [ _One, _Two , _Three | _Tail ] ->
+            many
+    end.
+                                                %case Value of
+                                                %[Head | Tail] ->
+                                                %   [string:to_upper(Head) | string:to_lower(Tail)];
+
+                                                %[] -> "Rafiki"
+                                                %end.
 
 
- %Trim =lists:map(fun(X) -> trim_after(Raw, X) == Raw end, [H|T]),
-  %lists:filter(fun(X) -> X == false end, Trim).
-%value_trim(Raw, [T]).
+                                                %function to loop through all the symbols to truncate after
+value(Raw) ->
+                                                %lists:all(fun erlang:is_integer/1, "123").
+                                                %X = lists:seq(1,100),
+    Tokens = string:tokens(string:trim(Raw), " "),
+                                                % V = fun (D) -> D >= $0 andalso D =< $9 end,
+    Z = lists:filter(fun(X) -> not lists:all(fun (D) -> D >= $0 andalso D =< $9 end, X)end, Tokens),
+                                                %Z = lists:filter(fun(X) -> lists:all(fun(A) -> is_integer(A)end, X) end, Tokens),
+                                                %V = lists:all(fun(X) -> is_integer(X)end, "123"),
+    io:format("~p~n", [Tokens]),
+    io:format("~s~n", [Z]).
 
-    %value_trim(V, []) -> V.
+                                                %lists:dropwhile(fun(X) ->lists:seq(X,100) end, Raw).
+                                                %[H |T] =string:tokens(string:trim(Raw), " "),
+                                                %case lists:dropwhile(fun(X) -> is_integer(X)end, H) of
+                                                % [] -> T;
+                                                %_ -> Raw
+                                                %end.
 
-%trim_after(X, TrimAfter) -> trim_after_flat(lists:flatten(X), TrimAfter).
-%trim_after_flat([C| Cs], TrimAfter) ->
-%  case lists:member(C, TrimAfter) of
- %   false -> [C | trim_after(Cs, TrimAfter)];
-  %  true -> []
-  %end;
-%trim_after_flat([], _) ->
- % [].
+                                                % TruncatedValue = lists:takewhile(fun(X) -> not lists:member(X, TrimAfter) end, Raw),
+                                                %TruncatedValue1 = tuple_to_list(lists:splitwith(fun(A) -> lists:member(A, TrimAfter) end, Raw)),
+                                                %      io:format("~p~n",[TruncatedValue]),
+                                                %     io:format("~p~n",[TruncatedValue1]).
+                                                %string:tokens(string:trim(TruncatedValue), " ").
+
+
+                                                %(lists:map(fun (X) -> case trim_after(Raw, X) == Raw of
+                                                % true -> [];
+                                                %false -> trim_after(Raw, X)
+                                                % end
+                                                % end, TrimAfter)).
+
+
+                                                %Trim =lists:map(fun(X) -> trim_after(Raw, X) == Raw end, [H|T]),
+                                                %lists:filter(fun(X) -> X == false end, Trim).
+                                                %value_trim(Raw, [T]).
+
+                                                %value_trim(V, []) -> V.
+
+                                                %trim_after(X, TrimAfter) -> trim_after_flat(lists:flatten(X), TrimAfter).
+                                                %trim_after_flat([C| Cs], TrimAfter) ->
+                                                %  case lists:member(C, TrimAfter) of
+                                                %   false -> [C | trim_after(Cs, TrimAfter)];
+                                                %  true -> []
+                                                %end;
+                                                %trim_after_flat([], _) ->
+                                                % [].
 
 
 trim_after(X, TrimAfter) -> trim_after_flat(lists:flatten(X), TrimAfter).
 trim_after_flat([H| T], TrimAfter) ->
-  case lists:member(H, TrimAfter) of
-    false -> [H | trim_after(T, TrimAfter)];
-    true -> []
-  end;
+    case lists:member(H, TrimAfter) of
+        false -> [H | trim_after(T, TrimAfter)];
+        true -> []
+    end;
 trim_after_flat([], _) ->
-  [].
+    [].
 
 
 
-%Flatten a list and reverse it
+                                                %Flatten a list and reverse it
 flatten(X) -> lists:reverse(flatten(X, [])).
 
 flatten([], Acc) -> Acc;
 flatten([H|T],Acc) when is_list(H) -> flatten(T, flatten(H,Acc));
 flatten([H|T], Acc) -> flatten(T, [H|Acc]).
 
-%uppercase_head(lists:map(fun zero_to_o/1, Username)).
-%uppercase_head([$o | Tail]) -> [$O | Tail]; uppercase_head(X) -> X.
-%zero_to_o($0) -> $o; zero_to_o(x) -> x.
+                                                %uppercase_head(lists:map(fun zero_to_o/1, Username)).
+                                                %uppercase_head([$o | Tail]) -> [$O | Tail]; uppercase_head(X) -> X.
+                                                %zero_to_o($0) -> $o; zero_to_o(x) -> x.
 
 
 startin(Str1) ->
-  Str3 = string:tokens(string:trim(Str1), " "),
-  %Check_member = lists:member("I", Str3),
-  ToFilter = lists:map(fun string:casefold/1,
-                       ["I","am", "Mimi", ".", "jina", "Langu" "ni", "naitwa", "," , "+",
-                "1", "2", "3", "4", "5", "6", "7", "8","9", "my", "name", "is", "40130"]),
-  Filtered_list = lists:filter(fun(X) -> not lists:member(string:casefold(X), ToFilter)end, Str3),
-  io:format("~p~n", [Filtered_list]).
+    Str3 = string:tokens(string:trim(Str1), " "),
+                                                %Check_member = lists:member("I", Str3),
+    ToFilter = lists:map(fun string:casefold/1,
+                         ["I","am", "Mimi", ".", "jina", "Langu" "ni", "naitwa", "," , "+",
+                          "1", "2", "3", "4", "5", "6", "7", "8","9", "my", "name", "is", "40130"]),
+    Filtered_list = lists:filter(fun(X) -> not lists:member(string:casefold(X), ToFilter)end, Str3),
+    io:format("~p~n", [Filtered_list]).
 
 to_truncate(Name) ->
-  V = lists:seq(1,100),
-  Symbols =lists:flatten([$!, $@, $?, $/,V]),
-  %Tokens = string:tokens(string:trim(Name), " "),
-  %List_to_truncate =lists:map(fun (X) -> X end, Symbols),
-  %List_to_truncate = ["!", "@", "?"],
-  %Strip_off = lists:filter(fun (X) -> lists:member(X, List_to_truncate)end, Tokens),
- % [H] = Strip_off,
-   string:trim(Name, trailing,Symbols ).
-    %io:format("~p~n", [V]).
+    V = lists:seq(1,100),
+    Symbols =lists:flatten([$!, $@, $?, $/,V]),
+                                                %Tokens = string:tokens(string:trim(Name), " "),
+                                                %List_to_truncate =lists:map(fun (X) -> X end, Symbols),
+                                                %List_to_truncate = ["!", "@", "?"],
+                                                %Strip_off = lists:filter(fun (X) -> lists:member(X, List_to_truncate)end, Tokens),
+                                                % [H] = Strip_off,
+    string:trim(Name, trailing,Symbols ).
+                                                %io:format("~p~n", [V]).
 
-%trim_after(X, TrimAfter) -> trim_after_flat(lists:flatten(X), TrimAfter).
+                                                %trim_after(X, TrimAfter) -> trim_after_flat(lists:flatten(X), TrimAfter).
 
-%trim_after_flat([C | Cs], TrimAfter) ->
- % case lists:member(C, TrimAfter) of
-  %  false -> [C | trim_after(Cs, TrimAfter)];
-   % true -> []
-  %end;
-%trim_after_flat([], _) ->
- % [].
+                                                %trim_after_flat([C | Cs], TrimAfter) ->
+                                                % case lists:member(C, TrimAfter) of
+                                                %  false -> [C | trim_after(Cs, TrimAfter)];
+                                                % true -> []
+                                                %end;
+                                                %trim_after_flat([], _) ->
+                                                % [].
 
 
 
-% lists:map(fun (Z) -> string:replace(Z, "0", o)end, Z).
+                                                % lists:map(fun (Z) -> string:replace(Z, "0", o)end, Z).
 
 zero_to_o(0) -> o;
 zero_to_o(x) -> x.
 
-%spacing in between characters
+                                                %spacing in between characters
 intersperse([C], _) -> [C];
 intersperse([C|Rest], Sep) -> [C | Sep] ++ intersperse(Rest, Sep).
--- optin.erl
+++ optin.erl
@@ -1,2 +1,151 @@
-- module(optin).
-- export([]).
+%%% @doc
+%%% This module tracks how many times the system has sent messages to the user and whether the have responded
+%%% then based on that you query them on whether they need any keep getting the messages or they can opt out
+%%% @end
+-module(optin).
+-export([optin_handler/0]).
+
+-import(survey_tk,
+[engswa/2,
+raw_rh/0, yesno_rh/0, keyword_rh/1, fractional_range_rh/2, integer_range_rh/2,
+save_as/1, inc_counter/1, set_var/2,
+send_airtime/1,
+next/1, next_or_timeout/2,
+keyword_logic/1, depends/2,
+listener/3, question/5,
+final_message/2, quiet_ending/1, dummy_message/3]).
+
+-spec optin_handler() -> service:paddy_app().
+
+optin_handler() ->
+	lists:flatten(
+		[
+			%Maize optin questions.
+			%A welcome blast(welblst) which is different for every crop
+			question(welblst,
+			engswa("Welcome back to MoA-INFO's Grow Better  messaging service. Would you like to learn about proper timing of your maize harvest?\nA. Yes\nB. No",
+				"Karibu tena kwa huduma ya MoA-INFO ya upanzi bora wa mahindi. Je, ungependa kujifunza jinsi ya kujua wakati sahihi wa kuvuna mahindi yako?\nA. Ndio\nB. La"),
+			24*60,
+			yesno_rh(),
+			keyword_logic([{"yes", reset_maize_idle1},
+				{"no", increment_maize_idle},
+				{invalid, expire},
+				{timeout, welblstr}])),
+			
+			%Reminder to optin to maize messages
+			% A welcome blast reminder(welblstr), which will be different for each crop
+			question(welblstr,
+				engswa("Hello again from MoA-INFO. Reply now to receive harvest timing advice for maize!\nA. Yes\nB. No",
+					"Hujambo tena kutoka MoA-INFO. Chukua hatua sasa ili upokee ushauri wa wakati wa kuvuna mahindi!\nA. Ndio\nB. La"),
+				24*60,
+				yesno_rh(),
+				keyword_logic([{"yes", reset_maize_idle1},
+					{"no", increment_maize_idle},
+					{invalid, expire},
+					{timeout, increment_maize_idle}])),
+			
+			%what question is to be asked at this point
+			question(welblst1,
+				engswa("Welcome back to MoA-INFO's Grow Better Maize messaging service. Would you like to learn about proper timing of your maize harvest?\nA. Yes\nB. No",
+					"Karibu tena kwa huduma ya MoA-INFO ya upanzi bora wa mahindi. Je, ungependa kujifunza jinsi ya kujua wakati sahihi wa kuvuna mahindi yako?\nA. Ndio\nB. La"),
+				24*60,
+				yesno_rh(),
+				keyword_logic([{"yes", reset_maize_idle1},
+					{"no", increment_maize_idle},
+					{invalid, expire},
+					{timeout, welblstr}])),
+			%what question is to be asked at this point
+			question(welblstr1,
+				engswa("Hello again from MoA-INFO. Reply now to receive harvest timing advice for maize!\nA. Yes\nB. No",
+					"Hujambo tena kutoka MoA-INFO. Chukua hatua sasa ili upokee ushauri wa wakati wa kuvuna mahindi!\nA. Ndio\nB. La"),
+				24*60,
+				yesno_rh(),
+				keyword_logic([{"yes", reset_maize_idle1},
+					{"no", increment_maize_idle},
+					{invalid, expire},
+					{timeout, increment_maize_idle}])),
+			
+			%maize idle count sets the value of the count
+			
+		%Call the reset_maize_idle1 when the user responds with a yes
+			dummy_message(reset_maize_idle1,
+			%If yes to the optin question set maize idle count to zero.
+				set_var({maize_idle_count}, 0),
+				next(mzhartim1)),
+			
+			%(Add the module used to eliminate redundancy here).
+			maize_harvest_timing:maize_harvest_timing(mzthanx),
+			
+			%If no or an expiry to the optin question response.
+			%idle counter is set to 1, then the maize optout selector is called and optin is set as 0
+			dummy_message(increment_maize_idle,
+			%sets inc_counter to one
+				inc_counter({maize_idle_count}),
+			%what happens to counts that are less than 1, they don't get any optout message
+				next(maize_optout_question_selector)),
+			
+			%decide if to ask optout (either hard or soft)
+			dummy_message(maize_optout_question_selector,
+				none,
+				depends(
+					[{maize_idle_count}, welblst, welblstr],
+					fun (Count, Optin1, Optin2) ->
+						case Count of
+							Num when Num >=5  	-> next(maize_optout_hard);
+							Num when Num >=3  	-> next(maize_optout_soft);
+							_ ->
+								case Optin1=="no" orelse Optin2=="no" of
+									true	-> next(maizeno);
+									false	-> next(expire)
+								end
+						end
+					end)),
+			
+			%Optout questions
+			question(maize_optout_soft,
+				engswa("Would you like to stop receiving regular maize cropping advice messages this season?\nA. Yes\nB. No",
+					"Ungependa kuacha kupokea ushauri mara kwa mara wa upanzi wa mahindi msimu huu?\nA. Ndio\nB. La"),
+				24*60,
+				yesno_rh(),
+				keyword_logic([{"yes", set_maize_optout},
+					{"no", reset_maize_idle2},
+					{invalid, expire},
+					{timeout, expire}])),
+			
+			%note: in this message the text says how many messages have been ignored
+			question(maize_optout_hard,
+				engswa("We've noticed that you have not responded to the last 5 MoA-INFO Grow Better Maize messages. Reply A to continue receiving these messages this season.",
+					"Tumegundua kuwa hujajibu maswali matano ya mwisho ya huduma ya MoA-INFO ya upanzi bora wa mahindi. Jibu A kuendelea kupokea ujumbe huu msimu huu."),
+				24*60,
+				raw_rh(),
+				next_or_timeout(reset_maize_idle2,set_maize_optout)),
+			
+			%If user elects to not optout then reset idle counter to zero"
+			dummy_message(reset_maize_idle2,
+				set_var({maize_idle_count}, 0),
+				next(maize_continue_message)),
+			
+			dummy_message(set_maize_optout,
+				set_var({maize_optin}, "no"),
+				next(maize_optout_message)),
+			
+			%Final questions
+			%1 - if user has seen content.
+			final_message(mzthanx,
+				engswa("Thanks for using MoA-INFO's Grow Better Maize service. We will follow-up in coming weeks with more harvest and post-harvest tips.",
+					"Asante kwa kutumia huduma ya MoA-INFO ya upanzi bora wa mahindi. Tutafuatilia wiki zijazo na vidokezo zaidi vya kuvuna na baada ya kuvuna.")),
+			%2 - if user does not see content, and is not asked about opting out.
+			final_message(maizeno,
+				engswa("Thanks for letting us know. We will follow-up in coming weeks with more harvest and post-harvest tips.",
+					"Asante kwa kutujulisha. Tutafuatilia wiki zijazo na vidokezo zaidi vya kuvuna na baada ya kuvuna.")),
+			%If user is asked to optout and does.
+			final_message(maize_optout_message,
+				engswa("Thanks. You can still receive our maize cropping advice by sending MENU to 40130 anytime.",
+					"Asante. Bado unaweza kupokea ushauri wetu wa upanzi wa mahindi kwa kutuma neno ORODHA kwa 40130 wakati wowote.")),
+			%If user is asked to optout and does not.
+			final_message(maize_continue_message,
+				engswa("Thanks. You will continue receiving our regular maize messages.",
+					"Asante. Utaendelea kupokea ujumbe wetu wa mara kwa mara kuhusu mahindi.")),
+			
+			quiet_ending(expire)
+		]).
