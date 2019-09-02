19782f9cdb54de7450c06c2417985c6d76194e7d
WanjikuMac <edwardscaroline32@gmail.com>
2019-09-02 15:02:36 +0300

    add columns to the design and cost tables

--- test_mnesia.erl
+++ test_mnesia.erl
@@ -1,5 +1,5 @@
 -module(test_mnesia).
--export([do_this_once/0, add_shop_item/3, remove_row/1]).
+-export([do_this_once/0, add_shop_item/3, remove_row/1, add_design/2, add_cost/2]).
 
 -record(shop, {item, quantity, cost}).
 -record(cost, {name, price}).
@@ -17,7 +17,24 @@ do_this_once() ->
 	mnesia:create_table(cost, [{attributes, record_info(fields, cost)}]),
 	mnesia:create_table(design,[{attributes, record_info(fields, design)}]),
 	mnesia:stop().
-	
+
+% add a row to the design table
+add_design(Name, Cost) ->
+	Row =#design{name =Name, cost = Cost},
+	F = fun() ->
+		mnesia:write(Row)
+			end,
+	mnesia:transaction(F).
+
+%add a row to the cost table
+add_cost(Name, Price) ->
+	Row = #cost{name = Name, price = Price},
+	F = fun() ->
+		mnesia:write(Row)
+			end,
+	mnesia:transaction(F).
+
+% add a column to the shop table
 add_shop_item(Name, Quantity, Cost) ->
 	Row = #shop{item = Name, quantity = Quantity, cost = Cost},
 	F =fun() ->
