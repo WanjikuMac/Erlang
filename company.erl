c86442148b38b61555b5692b08f98075deae6de4
WanjikuMac <edwardscaroline32@gmail.com>
2019-09-12 09:45:50 +0300

    minor changes

--- company.erl
+++ company.erl
@@ -1,5 +1,5 @@
 -module(company).
--export([setup_db/0, mk_projs/2, insert_emp/3, raise_salary/2, add_empl/6,raise_trial/2]).
+-export([setup_db/0, mk_projs/2, insert_emp/3, raise_salary/2, add_empl/6,raise_trial/2, read_emp_table/1]).
 
 -record(employee, {emp_no, name, salary, sex, phone, room_no}).
 -record(dept, {id, dept_name}).
@@ -9,11 +9,11 @@
 -record(in_proj, {emp, proj_name}).
 
 setup_db() ->
-    mnesia:create_table(employee, [{attributes, record_info(fields, employee)}]),
-    mnesia:create_table(dept, [{attributes, record_info(fields, dept)}]),
-    mnesia:create_table(project,[{attributes, record_info(fields, project)}]),
-    mnesia:create_table(manager, [{attributes, record_info(fields, manager)}]),
-    mnesia:create_table(at_dept, [{attributes, record_info(fields, at_dept)}]),
+    mnesia:create_table(employee, [{attributes, record_info(fields, employee)}, {disc_copies, [node(), nodes()]}]),
+    mnesia:create_table(dept, [{attributes, record_info(fields, dept)}, {disc_copies, [node(), nodes()]}]),
+    mnesia:create_table(project,[{attributes, record_info(fields, project)}, {disc_copies, [node(), nodes()]}]),
+    mnesia:create_table(manager, [{attributes, record_info(fields, manager)},{disc_copies, [node(), nodes()]} ]),
+    mnesia:create_table(at_dept, [{attributes, record_info(fields, at_dept)}, {disc_copies, [node(), nodes()]}]),
     mnesia:create_table(in_proj, [{attributes, record_info(fields, in_proj)}]).
 
 insert_emp(Emp, DeptId, ProjNames) ->
@@ -21,10 +21,16 @@ insert_emp(Emp, DeptId, ProjNames) ->
     F =fun() ->
                mnesia:write(Emp),
                AtDep = #at_dept{emp = Ename, dept_id = DeptId},
-               mnesia:write(AtDep),
-               mk_projs(Ename, ProjNames)
+               mnesia:write(AtDep)
+              %mk_projs(Ename, ProjNames)
        end,
     mnesia:transaction(F).
+
+read_emp_table(Name) ->
+  F = fun() ->
+    mnesia:read(employee, Name)
+      end,
+  mnesia:transaction(F).
 
 mk_projs(Ename, [ProjName | Tail]) ->
     V = #in_proj{emp = Ename, proj_name = ProjName},
