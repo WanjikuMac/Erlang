6449ded882e2ac5d716e5c671a87774d543eabf2
WanjikuMac <edwardscaroline32@gmail.com>
2019-11-25 09:29:21 +0300

    better understanding of the survey encompasses function

--- optin.erl
+++ optin.erl
@@ -3,7 +3,7 @@
 %%% then based on that you query them on whether they need any keep getting the messages or they can opt out
 %%% @end
 -module(optin).
--export([to_minute/1, survey_encompasses_minute/2, pred/1]).
+-export([to_minute/1, survey_encompasses_minute/2, pred/2]).
 
 -spec to_minute(datetime_micro()) -> calendar:datetime().
 -type datetime_micro() :: {{integer(), integer(), integer()}, {integer(), integer(), integer() | float()}}.
@@ -15,13 +15,14 @@ to_minute({Date, {HH, MM, _SS}})->
 survey_encompasses_minute({open, Values}, DateTime) ->
 	case proplists:lookup(opened, Values) of
 		none -> false;
-		{opened, OpendDT} -> to_minute(OpendDT) =< DateTime
+		{opened, OpendDT} -> to_minute(OpendDT) =< DateTime,
+			io:format("st_sync found unexpected value for Fields ~n")
 	end.
 
-pred({closed}) ->
-	case Value of
-		[{closed, [{opened, {{2019,10,25},{12,42,32.088486}}}] -> closed ;
-		
-	end
+pred(X, Y) ->
+	SUM = X +Y,
+	io:format("st_sync found unexpected value for Fields ~n"),
+		SUM.
+
 	
 
