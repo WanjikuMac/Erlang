3855b0a3092a6b9bea5142221c8d65338ec71ef1
WanjikuMac <edwardscaroline32@gmail.com>
2019-11-27 12:35:05 +0300

    added a counter to run a gen event

---
+++ webinar_examples/counter.erl
@@ -0,0 +1,21 @@
+-module(counter).
+-behavior(gen_event).
+
+-export([init/1,handle_event/2, handle_call/2, code_change/3, handle_info/2, terminate/2]).
+
+init(_Args) ->
+	io:put_chars("** 0: initialize to zero"),
+	{ok, 0}.
+
+handle_event(Event, Count) ->
+	io:format("** +1: got event ~p~n", [Event]),
+	{ok, Count + 1}.
+handle_call(Request, Count) ->
+	io:format("** got request ~p~n", [Request]),
+	{ok, Count, Count}.
+
+code_change(_OldVsn, State, _Extra) -> {ok, State}.
+
+handle_info(_Info, State) -> {noreply, State}.
+
+terminate(_Args, _State) -> ok.
