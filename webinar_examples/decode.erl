36c760fa6066f1dfa6740b7824b0dd19a32c706b
WanjikuMac <edwardscaroline32@gmail.com>
2020-01-03 08:39:14 +0300

    fixing my commits

---
+++ webinar_examples/decode.erl
@@ -0,0 +1,10 @@
+-module(decode).
+-export([]).
+
+% "{\"sid\": \"SMd6fe4a3a79444f21b209ac5634bf8e3b\", \"date_created\": \"Tue, 03 Dec 2019 07:40:09 +0000\", \"date_updated\": \"Tue, 03 Dec 2019 07:40:09 +0000\", \"date_sent\": null, \"account_sid\": \"AC3e7f76cad36f4ec370c5cecdbfeb99ed\", \"to\": \"whatsapp:+254718959202\", \"from\": \"whatsapp:+14155238886\",
+% \"messaging_service_sid\": null, \"body\": \"Hi! What is your name?\",\"status\": \"queued\", \"num_segments\": \"1\", \"num_media\": \"0\", \"direction\": \"outbound-api\", \"api_version\": \"2010-04-01\",
+% \"price\": null, \"price_unit\": null, \"error_code\": null, \"error_message\": null,\"uri\": \"/2010-04-01/Accounts/AC3e7f76cad36f4ec370c5cecdbfeb99ed/Messages/SMd6fe4a3a79444f21b209ac5634bf8e3b.json\"}
+
+%funct(Body) ->
+%jiffy:decode(Body),
+
