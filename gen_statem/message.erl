fc08814e4e192f6f415778e0ec3c06eda456f7f2
WanjikuMac <edwardscaroline32@gmail.com>
2020-01-15 13:54:49 +0300

    send message after 5 mins timeout

--- gen_statem/message.erl
+++ gen_statem/message.erl
@@ -2,11 +2,11 @@
 -behavior(gen_statem).
 -define(MESSAGE, message).
 
--export([init/1,callback_mode/0, has_message/3 ]).
--export([start/1, send_msg/0]).
+-export([init/1,callback_mode/0, has_message/3, waiting/3 ]).
+-export([start_link/1, send_msg/0]).
 
 %%client side routines
-start(Sms) ->
+start_link(Sms) ->
 	gen_statem:start({local,?MESSAGE}, ?MODULE, Sms, []).
 send_msg() ->
 	gen_statem:cast(?MESSAGE, has_message).
@@ -14,20 +14,31 @@ send_msg() ->
 
 
 %server side routines
-init(Sms)->
+init(Data)->
 	has_msg(),
-	Data = [Sms],
-	{ok, has_message, Data}.
+	[ _H | T] = Data ,
+	{ok, has_message, T}.
 
 callback_mode() ->
 	state_functions.
 
 %%state routines
 has_message(cast, has_message, Sms) ->
-	io:format("~p~n", [Sms]),
-	{next_state, waiting, Sms}.
+		[H | T]  = Sms,
+			io:format("~p~n", [H]),
+			{next_state, waiting, T, [{state_timeout, 5000, wait}]}.
 
+waiting(state_timeout, wait, []) ->
+	no_msg(),
+	{keep_state, []};
+waiting(state_timeout, wait, Data ) ->
+	[Head | Tail] = Data,
+	io:format("~p~n", [Head]),
+	{next_state, has_message , Tail}.
 
 %%helper functions
 has_msg() ->
-	io:format("Message Received~n", []).
+	io:format("Message List Received~n", []).
+no_msg() ->
+	io:format("List cleared~n", []).
+%["welcome", "karibu", "join", "dias"]
