9f727d89e209ad61985e4272ccb4f76b1e5641bf
WanjikuMac <edwardscaroline32@gmail.com>
2020-02-10 14:47:38 +0300

    message parsing

--- processes/world.erl
+++ processes/world.erl
@@ -2,6 +2,9 @@
 -export([start/0]).
 
 start() ->
-	spawn(person, init, ["Caroline"]),
-	spawn(dog, init, []),
-	spawn(rabbit, init, []).
+	Person = spawn(person, init, ["Caroline"]),
+	Dog = spawn(dog, init, []),
+	Rabbit = spawn(rabbit, init, []),
+	io:format("1st PID: ~p~n 2nd PID: ~p~n 3rd PID: ~p~n", [Person, Dog, Rabbit]),
+
+	Rabbit ! {self(), "Poor Rabbits run for you life"}.
