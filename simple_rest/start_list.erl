19e14272e68f4491702183099699191f5a89072e
WanjikuMac <edwardscaroline32@gmail.com>
2020-02-25 11:35:20 +0300

    add comments for easier understanding, this doesnt compile locally

--- simple_rest/start_list.erl
+++ simple_rest/start_list.erl
@@ -2,5 +2,10 @@
 Dispatch = cowboy_router:compile([{'_', [
 																		{"/", toppage_h, []}]
 																	}]),
+% A map containing all the options for the different protocols that  maybe involved
+% while connecting to the listener
 ProtocolOpts =  #{env => #{dispatch => Dispatch}}
-{ok, _} = cowboy:start_clear(example, [{port, 8080}], ProtocolOpts)
+%% Transport  options are where TCP options, including listener's port number are defined
+% Available options are documented in ranch_tcp(3)
+TransportOpts = [{port, 8080}]
+{ok, _} = cowboy:start_clear(example, TransportOpts , ProtocolOpts)
