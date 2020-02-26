bf0d1a98337854907264842cb8f406a12a69f92c
WanjikuMac <edwardscaroline32@gmail.com>
2020-02-26 17:42:38 +0300

    set up the http requests

--- simple_rest/src/simple_rest_app.erl
+++ simple_rest/src/simple_rest_app.erl
@@ -5,6 +5,12 @@
 -export([stop/1]).
 
 start(_Type, _Args) ->
+	Route = [{'_', [{"/", rest_time_handler, []}]}],
+	Dispatch = cowboy_router:compile(Route),
+	
+	ProtocolOpts = #{env =>  #{dispatch => Dispatch}},
+	TransportOpts = [{port, 8080}],
+	{ok, _} = cowboy:start_clear(my_http_listener, TransportOpts, ProtocolOpts),
 	simple_rest_sup:start_link().
 
 stop(_State) ->
