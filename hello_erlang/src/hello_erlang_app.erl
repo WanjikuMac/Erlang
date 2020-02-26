5f71a30bb6f9166e46c695ed9988867c138c6d7b
WanjikuMac <edwardscaroline32@gmail.com>
2020-02-26 12:35:17 +0300

    add comments to explain the routing concept

--- hello_erlang/src/hello_erlang_app.erl
+++ hello_erlang/src/hello_erlang_app.erl
@@ -5,8 +5,15 @@
 -export([stop/1]).
 
 start(_Type, _Args) ->
-	Dispatch = cowboy_router:compile([
-		{'_', [{"/", hello_handler, []}]}]),
+	% Routes take different structures im
+	% Routes = [Host1, Host2, ... HostN] the host contains matching rules
+	% for the host along with optional constrains and a list of routes for the path component
+	% In our case we are using
+	%           * Host = {HostMatch, PathList} => [{'_', Pathlist}].
+	% and for the path list
+	%           * Pathlist = [{PathMatch, Handler, InitialState}] => [{"/", hello_handler, []}].
+	Routes = [{ '_', [{"/", hello_handler, []}] }],
+	Dispatch = cowboy_router:compile(Routes),
 	ProtocolOpts = #{env => #{dispatch => Dispatch}},
 	{ok, _ } = cowboy:start_clear(my_http_listener, [{port,8080}], ProtocolOpts),
 	hello_erlang_sup:start_link().
