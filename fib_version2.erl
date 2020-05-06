24d8e2acb60cae74ee973e74c05e0115b73c1201
WanjikuMac <edwardscaroline32@gmail.com>
2020-05-06 12:20:32 +0300

    add print statements to better understand the code

--- fib_version2.erl
+++ fib_version2.erl
@@ -1,10 +1,12 @@
 -module(fib_version2).
--export([main/0]).
+-export([main/0, fib/3]).
 
 main() ->
     Answer = fib(2,1,0),
     io:format("Answer ~p~n", [Answer]).
+
 fib(A, B, S) when A >= 100, B rem 2 =:= 0 -> B + S;
+%io:format("A: ~p~n B: ~p~n C: ~p~n", [A, B, S]);
 fib(A, _, S) when A >= 100 -> S;
 fib(A,B,S) when B rem 2 =:= 0 -> fib(A +B, A, B+S);
 fib(A,B,S) -> fib(A +B, A, S).
