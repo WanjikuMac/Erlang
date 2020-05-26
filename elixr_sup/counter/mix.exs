3a5d19a56ff550fabc627ab83e30fa2cd0389f8d
WanjikuMac <edwardscaroline32@gmail.com>
2020-05-26 12:21:36 +0300

    test and supervison deps

--- elixr_sup/counter/mix.exs
+++ elixr_sup/counter/mix.exs
@@ -12,6 +12,7 @@ defmodule Counter.MixProject do
   end
 
   # Run "mix help compile.app" to learn about applications.
+  # Applications we depend on and all the supervisors we are going to start up
   def application do
     [
       extra_applications: [:logger],
@@ -20,6 +21,7 @@ defmodule Counter.MixProject do
   end
 
   # Run "mix help deps" to learn about dependencies.
+  # Build time dependancies
   defp deps do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
